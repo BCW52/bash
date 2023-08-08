6fe75e5786ed32a6e521c7d00440183d110d3643
BCW52 <nahidislam420gamer@gmail.com>
2023-08-08 13:52:14 +0600

    Upgrade and mqilcow removed

--- set.sh
+++ set.sh
@@ -1,9 +1,7 @@
 cd /opt
 
 		git clone https://github.com/knadh/listmonk.git
-      sudo apt install wget ca-certificates
-      sudo apt update 
-      sudo apt upgrade 
+      sudo apt install wget ca-certificate
       sudo apt install docker docker-compose
       sudo apt install postgresql postgresql-contrib 
 	  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - 
@@ -15,11 +13,9 @@ cd /opt
 	  cd /opt
       sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
       sudo chmod +x /usr/local/bin/docker-compose
-	 git clone https://github.com/mailcow/mailcow-dockerized
-cd mailcow-dockerized
-./generate_config.sh
-docker-compose pull
-docker-compose up -d
+
 	        cd listmonk
       sudo chmod +x install-prod.sh
       ./install-prod.sh
+docker-compose pull
+docker-compose up -d
